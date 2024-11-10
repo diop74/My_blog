ba336c48db448b5b4de521136a22b92e47a3f9fe
diop74 <diopoumar03@gmail.com>
2024-11-10 18:43:44 +0000

    pages et routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/about';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Dashboard from './pages/dashboard';
+import Projects from './pages/Projects';
 
 const App = () => {
   return (
-    <div>
-      Here we go again!
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            whays up
+        </div>
+    );
+}
+
+export default Dashboard;
