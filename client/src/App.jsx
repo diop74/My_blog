42b009f7919a46fdf2211ed7a6485b7636837ae9
diop74 <diopoumar03@gmail.com>
2024-11-11 00:00:28 +0000

    Add Header component and update page content; modify routes and dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import ProjectsPage from './pages/Projects';
+import Header from './components/Header';
+import About from './pages/about';
+import Dashboard from './pages/dashboard';
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      Here we go again!
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
