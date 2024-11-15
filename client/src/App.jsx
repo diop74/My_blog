46aaec209431aa6eadca5ce8cb727ad8e5784e21
diop74 <diopoumar03@gmail.com>
2024-11-15 19:49:46 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/about';
+import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
